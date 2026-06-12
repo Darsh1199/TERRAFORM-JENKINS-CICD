290cffe2071c4ddebe39ce56f1fb680944b2c6f0
Darsh1199 <143002094+Darsh1199@users.noreply.github.com>
2026-06-12 16:37:25 +0530

    Update1 provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,12 @@
 terraform {
+  required_version = ">= 1.2.0"
+
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
     }
   }
-  required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region = var.aws_region
-}
+provider "docker" {}
