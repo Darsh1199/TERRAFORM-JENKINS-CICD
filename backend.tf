03652651fd7476cdf7ebe0ca3c6bd2fea1c477e5
Darsh1199 <143002094+Darsh1199@users.noreply.github.com>
2026-06-12 16:20:24 +0530

    Update1 backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,16 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
-    key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    bucket = "mrcloudbook-cicd-bucket"
+    key    = "my-terraform-environment/main"
+    region = "us-east-1"
+
+    endpoints = {
+      s3 = "http://localhost:9001"
+    }
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style              = true
   }
 }
