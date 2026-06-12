ece0c8481a45b5a2509f122de31e973778497bf0
Darsh1199 <143002094+Darsh1199@users.noreply.github.com>
2026-06-12 16:41:56 +0530

    Update1 main.tf

--- main.tf
+++ main.tf
@@ -1,61 +1,13 @@
-resource "aws_instance" "Ajay" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  user_data              = base64encode(file("website.sh"))
-  tags = {
-    Name = "Aj-EC2"
-  }
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
 }
 
-resource "aws_security_group" "ec2_security_group" {
-  name        = "ec2 security group"
-  description = "allow access on ports 80 and 22 and 443"
+resource "docker_container" "web" {
+  name  = "web-server"
+  image = docker_image.nginx.image_id
 
-  ingress {
-    description      = "ssh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    from_port   = 0
-    to_port     = 0    # Allow all ports
-    protocol    = "-1" # All protocols
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description      = "https"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description      = "http"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "Aj_sg"
+  ports {
+    internal = 80
+    external = 8080
   }
 }
